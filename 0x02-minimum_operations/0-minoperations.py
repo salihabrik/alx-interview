4ba350381e87dc5cfe68802faf6db72e383739aa
salihabrik <briksaliha@gmail.com>
2024-01-16 21:46:45 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -20,19 +20,12 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    dp = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        if dp[i] == 0:
-            dp[i] = dp[i - 1] + 1
-            for j in range(i * 2, n + 1, i):
-                dp[j] = dp[i] + dp[j // i]
-
-    return dp[n]
-
-if __name__ == "__main__":
-    n = 4
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    operations = 0
+    divisor = 2
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n /= divisor
+        else:
+            divisor += 1
+    return operations
