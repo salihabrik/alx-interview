d9b2ea61b876025a758a485b9475b9726ebb120b
salihabrik <briksaliha@gmail.com>
2024-01-18 21:54:31 +0100

    minoperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,31 +1,21 @@
+#!/usr/bin/python3
+"""
+Module for minimum operations problem
+"""
+
+
 def minOperations(n):
     """
-<<<<<<< HEAD
     Calculates the fewest number of operations needed to result in exactly n H characters in the file.
-
-    :param n: Integer, the target number of H characters
-    :return: Integer, the fewest number of operations needed
-=======
-    Calculates the fewest number of operations needed to result in exactly n H characters in file.
->>>>>>> 3156ffb62988833b2c4c5b23ede4ae559645d62c
     """
     if n <= 1:
         return 0
-
     operations = 0
     divisor = 2
-
     while n > 1:
-        while n % divisor == 0:
+        if n % divisor == 0:
             operations += divisor
-            n //= divisor
-        divisor += 1
-
+            n /= divisor
+        else:
+            divisor += 1
     return operations
-
-# Example test cases
-n1 = 4
-print("Min # of operations to reach {} char: {}".format(n1, minOperations(n1)))
-
-n2 = 12
-print("Min # of operations to reach {} char: {}".format(n2, minOperations(n2)))
